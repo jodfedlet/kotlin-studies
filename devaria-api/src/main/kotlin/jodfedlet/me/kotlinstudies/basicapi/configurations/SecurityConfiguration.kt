ac8d2a7865d66c57b4ec438ce29b3328dc67bb24
Jod Fedlet Pierre <80819521@redecorp.br>
2022-09-21 14:27:53 -0300

    get token with auth user.

--- devaria-api/src/main/kotlin/jodfedlet/me/kotlinstudies/basicapi/configurations/SecurityConfiguration.kt
+++ devaria-api/src/main/kotlin/jodfedlet/me/kotlinstudies/basicapi/configurations/SecurityConfiguration.kt
@@ -1,6 +1,7 @@
 package jodfedlet.me.kotlinstudies.basicapi.configurations
 
 import jodfedlet.me.kotlinstudies.basicapi.filters.JWTAuthorizingFilter
+import jodfedlet.me.kotlinstudies.basicapi.repositories.UserRepository
 import jodfedlet.me.kotlinstudies.basicapi.utils.JWTUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
@@ -17,13 +18,16 @@ class SecurityConfiguration: WebSecurityConfigurerAdapter() {
     @Autowired
     private lateinit var  jwtUtils: JWTUtils
 
+    @Autowired
+    private lateinit var usersRepository: UserRepository
+
     override fun configure(http: HttpSecurity) {
         http.csrf().disable().authorizeRequests()
             .antMatchers(HttpMethod.POST,"/api/auth/token").permitAll()
             .antMatchers(HttpMethod.POST,"/api/users").permitAll()
             .anyRequest().authenticated()
 
-        http.addFilter(JWTAuthorizingFilter(authenticationManager(), jwtUtils))
+        http.addFilter(JWTAuthorizingFilter(authenticationManager(), jwtUtils, usersRepository))
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
 }
